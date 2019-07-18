7854fd9f5ab2c7d18203a14699fe4f68786880ae
Brian <brianfee7@gmail.com>
2019-07-17 20:57:29 -0600

    Better access to class variables. Added weight to Logic class.

--- vulcan_logic/core.py
+++ vulcan_logic/core.py
@@ -3,24 +3,65 @@
 class Logic:
     """ A logic string container. """
 
-    def __init__(self, logic=None):
+    def __init__(self, logic=None, weight=None):
         """ Logic class initialization. """
-        self.logic_matrix = self.set_logic_matrix(logic)
+        self.__logic_matrix = None
+        self.__weight = None
+
+        self.logic = logic
+        self.weight = weight
+
+
+    def __repr__(self):
+        return self.to_eval_string()
 
 
 
     def __str__(self):
-        """ Prints the logic_matrix in a readable format. """
+        """ Prints the __logic_matrix in a readable format. """
         print_str = ''
-        for row in self.logic_matrix:
+        for row in self.__logic_matrix:
             print_str += row['left'] + ' '
             print_str += row['eval'] + ' '
             print_str += row['right'] + '\n'
-        return print_str
+        return print_str[:-1] # Trim final newline
 
 
 
-    def set_logic_matrix(self, logic):
+    @property
+    def weight(self):
+        """ Gets or sets the weight of the Logic String. """
+        return self.__weight
+
+
+
+    @weight.setter
+    def weight(self, weight):
+        return self._set_weight(weight)
+
+
+
+    def _set_weight(self, weight=None):
+        """ Calculates the weight of the Logic string.
+
+        By default, Logic weight is calculated as the number of equations
+        within the logic string. Future versions should allow field names
+        to contribute varying weights.
+        """
+
+        self.__weight = len(self.__logic_matrix) if weight is None else weight
+
+
+
+    @property
+    def logic(self):
+        """ Gets or sets the weight of the Logic String. """
+        return self.__logic_matrix
+
+
+
+    @logic.setter
+    def logic(self, logic):
         """ Takes a logic string and expands it into an n x 3 matrix. """
 
         equations = [equation.strip() for equation in logic.split(',')]
@@ -28,7 +69,7 @@ class Logic:
         for equation in equations:
             equations_matrix.append(self.split_equation(equation))
 
-        return equations_matrix
+        self.__logic_matrix = equations_matrix
 
 
 
@@ -54,9 +95,9 @@ class Logic:
     def to_eval_string(self):
         """ Converts the logic matrix into a string for eval(). """
 
-        eval_string = None if not self.logic_matrix else ''
+        eval_string = None if not self.__logic_matrix else ''
 
-        for i, row in enumerate(self.logic_matrix):
+        for i, row in enumerate(self.__logic_matrix):
             if i > 0:
                 eval_string += ' and '
 
